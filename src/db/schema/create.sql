417f2352f5ac88fa8e488d836dc8e3e47a4386dc
Dmitry <dmitry.petrov0918@gmail.com>
2020-08-03 13:11:04 -0400

    Updated the tables in the schema

--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -1,36 +1,34 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS recycling CASCADE;
-DROP TABLE IF EXISTS organics CASCADE;
-DROP TABLE IF EXISTS garbage CASCADE;
-DROP TABLE IF EXISTS user_waste CASCADE;
+DROP TABLE IF EXISTS waste CASCADE;
+DROP TABLE IF EXISTS users_waste CASCADE;
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  score INTEGER
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  -- "full_name" varchar,
+  "email" varchar,
+  "password" password
 );
 
-CREATE TABLE recycling (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE "waste" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar
 );
 
-CREATE TABLE organics (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE "users_waste" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+  "waste_id" INTEGER UNIQUE REFERENCES waste(id) ON DELETE CASCADE,
+  "score" INTEGER
 );
 
-CREATE TABLE garbage (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
-);
+-- ALTER TABLE "users_waste" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-CREATE TABLE user_waste (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  recycling_id INTEGER UNIQUE REFERENCES recycling(id) ON DELETE CASCADE,
-  organic_id INTEGER UNIQUE REFERENCES organics(id) ON DELETE CASCADE,
-  garbage_id INTEGER UNIQUE REFERENCES garbage(id) ON DELETE CASCADE
-);
+-- ALTER TABLE "users_waste" ADD FOREIGN KEY ("waste_id") REFERENCES "waste" ("id");
+
+-- CREATE TABLE user_waste (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   recycling_id INTEGER UNIQUE REFERENCES recycling(id) ON DELETE CASCADE,
+--   organic_id INTEGER UNIQUE REFERENCES organics(id) ON DELETE CASCADE,
+--   garbage_id INTEGER UNIQUE REFERENCES garbage(id) ON DELETE CASCADE
+-- );
