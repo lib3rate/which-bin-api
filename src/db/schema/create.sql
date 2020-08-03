418f3a0b4363bf1ecae9b0c5c0a51cede07a0da4
Dmitry <dmitry.petrov0918@gmail.com>
2020-08-03 13:41:44 -0400

    Update the seed files

--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -1,34 +1,22 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS waste CASCADE;
-DROP TABLE IF EXISTS users_waste CASCADE;
+DROP TABLE IF EXISTS bins CASCADE;
+DROP TABLE IF EXISTS user_bins CASCADE;
 
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  -- "full_name" varchar,
+  "username" varchar,
   "email" varchar,
-  "password" password
+  "password" varchar
 );
 
-CREATE TABLE "waste" (
+CREATE TABLE "bins" (
   "id" SERIAL PRIMARY KEY,
   "name" varchar
 );
 
-CREATE TABLE "users_waste" (
+CREATE TABLE "user_bins" (
   "id" SERIAL PRIMARY KEY,
-  "user_id" INTEGER UNIQUE REFERENCES users(id) ON DELETE CASCADE,
-  "waste_id" INTEGER UNIQUE REFERENCES waste(id) ON DELETE CASCADE,
+  "user_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "bin_id" INTEGER REFERENCES bins(id) ON DELETE CASCADE,
   "score" INTEGER
 );
-
--- ALTER TABLE "users_waste" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
--- ALTER TABLE "users_waste" ADD FOREIGN KEY ("waste_id") REFERENCES "waste" ("id");
-
--- CREATE TABLE user_waste (
---   id SERIAL PRIMARY KEY NOT NULL,
---   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
---   recycling_id INTEGER UNIQUE REFERENCES recycling(id) ON DELETE CASCADE,
---   organic_id INTEGER UNIQUE REFERENCES organics(id) ON DELETE CASCADE,
---   garbage_id INTEGER UNIQUE REFERENCES garbage(id) ON DELETE CASCADE
--- );
--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -1,132 +1,35 @@
-INSERT INTO users (name, email, password, score)
+INSERT INTO users (username, email, password)
 VALUES
-  ('Sylvia Palmer',   'Sylvia.Palmer@erich.biz',  '1234', 0),
-  ('Tori Malcolm',    'tori@malcolm.com',         '1234', 0),
-  ('Mildred Nazir',   'mildred@nazir.com',        '1234', 0),
-  ('Cohana Roy',      'cohana@roy.com',           '1234', 0),
-  ('Sven Jones',      'sven@jones.com',           '1234', 0),
-  ('Susan Reynolds',  'susan@reynolds.com',       '1234', 0),
-  ('Alec Quon',       'alec@quon.com',            '1234', 0),
-  ('Viktor Jain',     'victor@jain.com',          '1234', 0),
-  ('Lindsay Chu',     'lindsay.chu@email.biz',    '1234', 0),
-  ('Samantha Stanic', 'sam.stanic@erich.biz',     '1234', 0);
+  ('Sylvia Palmer',   'Sylvia.Palmer@erich.biz',  '1234'),
+  ('Tori Malcolm',    'tori@malcolm.com',         '1234'),
+  ('Mildred Nazir',   'mildred@nazir.com',        '1234'),
+  ('Cohana Roy',      'cohana@roy.com',           '1234'),
+  ('Sven Jones',      'sven@jones.com',           '1234'),
+  ('Susan Reynolds',  'susan@reynolds.com',       '1234'),
+  ('Alec Quon',       'alec@quon.com',            '1234'),
+  ('Viktor Jain',     'victor@jain.com',          '1234'),
+  ('Lindsay Chu',     'lindsay.chu@email.biz',    '1234'),
+  ('Samantha Stanic', 'sam.stanic@erich.biz',     '1234');
 
-INSERT INTO recycling (name)
+INSERT INTO bins (name)
 VALUES
-  ('Glass'),
-  ('Plastic'),
-  ('Aluminum'),
-  ('Plastic Bottle'),
-  ('Plastic Fork'),
-  ('Soda Can'),
-  ('Book'),
-  ('Paper'),
-  ('Mason Jar'),
-  ('Paper Envelope');
-   
-INSERT INTO organics (name)
+  ('organic'),
+  ('recycling'),
+  ('garbage');
+
+-- SELECT sum(score) FROM user_bins
+-- WHERE user_id = 2;
+
+INSERT INTO user_bins (user_id, bin_id, score)
 VALUES
-  ('banana peals'),
-  ('tomatoes'),
-  ('egg shells'),
-  ('apples'),
-  ('chicken bones'),
-  ('beef bones'),
-  ('coffee grounds'),
-  ('cheese'),
-  ('fish bones'),
-  ('tea bag');
-
-INSERT INTO garbage (name)
-VALUES
-  ('Fabric'),
-  ('Black Plastic Container'),
-  ('Hot Drink Cup'),
-  ('Wrapping Paper'),
-  ('Styrofoam'),
-  ('Cigarette Butt'),
-  ('Light Bulb'),
-  ('Gum Wrapper'),
-  ('Straw'),
-  ('Milk Box');
-
--- INSERT INTO user_waste (user_id, recycling_id, organics_id, garbage_id)
--- VALUES
---   -- Sylvia Palmer recycled Plastic bottle
---   (1, 4, NULL, NULL),
---   ('Black Plastic Container'),
---   ('Hot Drink Cup'),
---   ('Wrapping Paper'),
---   ('Styrofoam'),
---   ('Cigarette Butt'),
---   ('Light Bulb'),
---   ('Gum Wrapper'),
---   ('Straw'),
---   ('Milk Box');
-
--- INSERT INTO user_waste (user_id, organics_id)
--- VALUES
---   -- Sylvia Palmer has Plastic bottle
---   (1, 7),
---   ('Black Plastic Container'),
---   ('Hot Drink Cup'),
---   ('Wrapping Paper'),
---   ('Styrofoam'),
---   ('Cigarette Butt'),
---   ('Light Bulb'),
---   ('Gum Wrapper'),
---   ('Straw'),
---   ('Milk Box');
-
--- INSERT INTO available_interviewers (day_id, interviewer_id)
--- SELECT 1 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
-
--- INSERT INTO available_interviewers (day_id, interviewer_id)
--- SELECT 2 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
-
--- INSERT INTO available_interviewers (day_id, interviewer_id)
--- SELECT 3 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
-
--- INSERT INTO available_interviewers (day_id, interviewer_id)
--- SELECT 4 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
-
--- INSERT INTO available_interviewers (day_id, interviewer_id)
--- SELECT 5 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
-
--- WITH
--- appointments AS (
---   SELECT id as appointment_id, day_id FROM appointments ORDER BY RANDOM() LIMIT 10
--- ),
--- students(name) AS(
---   VALUES
---     ('Liam Martinez'),
---     ('Richard Wong'),
---     ('Lydia Miller-Jones'),
---     ('Archie Cohen'),
---     ('Chad Takahashi'),
---     ('Leopold Silvers'),
---     ('Maria Boucher'),
---     ('Jamal Jordan'),
---     ('Michael Chan-Montoya'),
---     ('Yuko Smith')
--- )
-
--- INSERT INTO interviews (student, appointment_id, interviewer_id)
--- SELECT
---   DISTINCT ON 
---   (s.name) name,
---   a.appointment_id AS appointment_id,
---   available_interviewers.interviewer_id AS interviewer_id
--- FROM (
---   SELECT
---     *, row_number() OVER(ORDER BY appointment_id) AS rnum
---   FROM appointments
--- ) AS a
--- JOIN (
---   SELECT
---     *, row_number() OVER(ORDER BY name) AS rnum
---   FROM students
--- ) AS s
--- ON a.rnum = s.rnum
--- JOIN available_interviewers
--- ON a.day_id = available_interviewers.day_id;
+  (1, 1, 2),
+  (2, 3, 2),
+  (3, 2, 3),
+  (4, 3, 2),
+  (5, 2, 1),
+  (6, 2, 1),
+  (10, 2, 1),
+  (9, 3, 2),
+  (10, 2, 2),
+  (7, 2, 2),
+  (8, 2, 3);
