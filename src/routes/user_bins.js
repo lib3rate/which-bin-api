328b28e8e7f9bb700cdd91c99b1345e645ab6d46
Dmitry <dmitry.petrov0918@gmail.com>
2020-08-04 10:12:45 -0400

    Update the query for GET user_bins

--- src/routes/user_bins.js
+++ src/routes/user_bins.js
@@ -4,8 +4,20 @@ module.exports = (db) => {
   router.get("/user_bins", (request, response) => {
     db.query(
       `
-      SELECT * FROM user_bins;
-    `
+        SELECT *
+        FROM user_bins
+        JOIN users ON user_id = users.id
+        ORDER BY score DESC
+        LIMIT 10;
+      `
+      // `
+      //   SELECT users.username, user_bins.id, sum(user_bins.score) as score
+      //   FROM user_bins
+      //   JOIN users ON user_id = users.id
+      //   GROUP BY user_bins.user_id
+      //   ORDER BY score DESC
+      //   LIMIT 10;
+      // `
     ).then(({ rows: user_bins }) => {
       response.json(
         user_bins.reduce(
