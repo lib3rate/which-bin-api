65e1678609ca67befadcdaa47b8ed3277f773e43
Dmitry <dmitry.petrov0918@gmail.com>
2020-08-08 23:58:12 -0400

    Add username to the GET /users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,12 +4,12 @@ module.exports = (db) => {
   router.get("/users", (request, response) => {
     db.query(
       `
-      SELECT user_id, bins.name, sum(user_bins.score)
+      SELECT users.username, user_id, bins.name, sum(user_bins.score)
       FROM user_bins
       JOIN users ON user_bins.user_id = users.id
       JOIN bins ON bins.id = user_bins.bin_id
       WHERE users.id = 3
-      GROUP BY user_id, bins.name;
+      GROUP BY users.username, user_id, bins.name;
     `
     ).then(({ rows: user }) => {
       response.json(user);
